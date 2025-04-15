82c09a24a0e0c49d5f0acd82766f8369e006a8f0
Pedrokeita <pedrolucas300805@gmail.com>
2025-04-15 09:57:47 -0300

    Initial commit

---
+++ fmea-manager/src/main/java/com/fmea/fmea_manager/FmeaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.fmea.fmea_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmeaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmeaManagerApplication.class, args);
+	}
+
+}
---
+++ fmea-manager/src/test/java/com/fmea/fmea_manager/FmeaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmea.fmea_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmeaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
